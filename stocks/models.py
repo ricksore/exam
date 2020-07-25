a80af5186a60d3639c0e5e5c421deda711981c52
wreck <rickalomiasore@gmail.com>
2020-07-25 13:43:10 +0800

    Increased length of stock name to 50

--- stocks/models.py
+++ stocks/models.py
@@ -7,7 +7,7 @@ class Stock(models.Model):
     """
 
     id = models.CharField(max_length=20, primary_key=True, unique=True)
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=50)
     price = models.FloatField()
     
     def __str__(self):
